d8df4d23b57bda06d0ded3937452adc11398fcfa
xRoier <58123692+xRoier@users.noreply.github.com>
2020-09-12 23:29:03 +0200

    Oopsie

--- Real096/Plugin.cs
+++ Real096/Plugin.cs
@@ -21,7 +21,7 @@ namespace Real096
         public override string Name => "Real096";
         public override string Author => "xRoier";
         public EventHandlers EventHandlers;
-        public override Version Version { get; } = new Version(2, 0, 0);
+        public override Version Version { get; } = new Version(1, 0, 0);
         public override Version RequiredExiledVersion { get; } = new Version(2, 1, 3);
         public override void OnEnabled()
         {
